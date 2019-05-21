17cea9f3c795cab90e72c3ed577b8a86c13b79fb
charles degrassat <charles.degrassat@gmail.com>
2019-05-21 17:57:04 +0200

    step 1

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,12 +8,17 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-    @dose.save
-    new
-    redirect_to cocktail_path(@cocktail)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
-  def delete
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
